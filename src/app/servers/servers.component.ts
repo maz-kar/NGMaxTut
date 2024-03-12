7663fe3d1e3079a22d0418265ec000fb28e4935d
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-03-12 21:10:21 +0100

    inline approach for template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  //templateUrl: './servers.component.html',
+  template: '<app-server></app-server><app-server></app-server>', 
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
